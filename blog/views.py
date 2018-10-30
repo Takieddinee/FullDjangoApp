70a12a9c54cbee526f6cc7f6df655e50eae0e43c
Takieddinee <et_ould_said@esi.dz>
2018-10-30 23:34:57 +0100

    Adding some route to the blog app, and make it as the priciple page.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1> this is home </h1>")
+
+
+def about(request):
+    return HttpResponse("<h1> this is About </h1>")
