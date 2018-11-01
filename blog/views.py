ae4f84747a997c17797960ad93c472f39339a83f
Takieddinee <et_ould_said@esi.dz>
2018-11-01 16:39:15 +0100

    Working with templates: Extension, static files, named route and adding bootstrap

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Fake data
+
+posts = [
+    {
+        'author': 'Takie',
+        'title': 'Title1',
+        'content': 'qsdmklgjqsdmlkgjqsmdlgkjqdsg',
+        'date_posted': 'August 9, 1995'
+    },
+    {
+        'author': 'Takie',
+        'title': 'Title2',
+        'content': 'qsdmklgjqsdmlkgjqsmdlgkjqdsg',
+        'date_posted': 'August 9, 1995'
+    }, {
+        'author': 'Takie',
+        'title': 'Title3',
+        'content': 'qsdmklgjqsdmlkgjqsmdlgkjqdsg',
+        'date_posted': 'August 9, 1995'
+    }
+]
+
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse("<h1> this is home </h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1> this is About </h1>")
+    return render(request, 'blog/about.html')
