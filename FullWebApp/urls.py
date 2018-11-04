d224c2739aa0e03276909cc87edd2a0258d6b057
Taki Eddine OULD SAID <et_ould_said@esi.dz>
2018-11-04 21:01:53 +0100

    User registration implementation with a specific form and using flash messages

--- FullWebApp/urls.py
+++ FullWebApp/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('register/', userViews.register, name="register"),
 
 ]
---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterationForm
+
+
+# Create your views here.
+def register(request):
+    if (request.method == "POST"):
+        form = UserRegisterationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'User {username} successfully added !')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterationForm()
+    return render(request, "users/register.html", {'form': form})
