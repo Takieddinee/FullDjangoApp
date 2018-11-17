eca42ac037626c2cbc5b96b424ab1960868f4f86
Taki Eddine OULD SAID <et_ould_said@esi.dz>
2018-11-17 18:51:06 +0100

    Reset Password

--- FullWebApp/settings.py
+++ FullWebApp/settings.py
@@ -126,4 +126,11 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "youremail"
+EMAIL_HOST_PASSWORD = "yourpassword"
+
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
